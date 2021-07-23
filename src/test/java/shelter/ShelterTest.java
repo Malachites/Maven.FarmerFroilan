acfb5d1f452fc502e2c32f17380b4f67b55f7476
Dipinti <dipinti@zipcodes-MBP.fios-router.home>
2021-07-23 10:32:13 -0400

    added shelter test

--- src/test/java/shelter/ShelterTest.java
+++ src/test/java/shelter/ShelterTest.java
@@ -2,13 +2,21 @@ package shelter;
 
 import animal.Chicken;
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class ShelterTest extends TestCase {
-
-    public void testSize() {
+    @Test
+    public void testAddChickens() {
         //given
+        int expectedValue=1;
         Chicken chicken=new Chicken("BooBooChickens");
         ChickenCoop newChickenCoop=new ChickenCoop();
+        //when
+        newChickenCoop.add(chicken);
+        int actualValue=newChickenCoop.size();
+        //then
+        Assert.assertEquals(expectedValue,actualValue);
     }
 
     public void testAdd() {
