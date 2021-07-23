0a1a911c4de960126a8d68ae95feb04f7a303801
laura <laurab90>
2021-07-22 21:44:27 -0400

    farm things

--- src/main/java/farm/CropRow.java
+++ src/main/java/farm/CropRow.java
@@ -1,4 +1,38 @@
 package farm;
 
-public class CropRow {
-}
+import crop.Crop;
+
+import java.util.ArrayList;
+
+public class CropRow< SomeTypeOfCrop extends Crop> {
+
+        ArrayList<SomeTypeOfCrop> crops;
+
+        public CropRow() {
+            crops = new ArrayList<SomeTypeOfCrop>();
+        }
+
+        public Integer getSize() {
+            return crops.size();
+        }
+
+        public void addCropRow(SomeTypeOfCrop crop) {
+            crops.add(crop);
+        }
+
+        public void removeCrop(SomeTypeOfCrop crop) {
+            crops.remove(crop);
+        }
+
+        public void removeAllCrops() {
+            crops.clear();
+
+        }
+        public ArrayList<SomeTypeOfCrop> getCropRow() {
+            return crops;
+        }
+
+
+
+    }
+
--- src/main/java/farm/Field.java
+++ src/main/java/farm/Field.java
@@ -1,4 +1,19 @@
 package farm;
 
+import java.util.HashMap;
+
 public class Field {
+    HashMap<String, Integer> storesCropRows = new HashMap<>();
+
+    public Field(){
+        storesCropRows = new HashMap<>();
+    }
+
+    public void addCropRow (CropRow cropRow){
+
+    }
+
+    public void removeCropRow(CropRow cropRow){
+
+    }
 }
