e1db3552214fca2622ed3b859ad6adfae4d8a78d
Raymond <raymond@zipcoder.local>
2021-07-22 20:23:33 -0400

    i dont want to mess anything up

--- src/main/java/vehicle/Vehicle.java
+++ src/main/java/vehicle/Vehicle.java
@@ -4,24 +4,30 @@ import Interface.NoiseMaker;
 import Interface.Rideable;
 import Interface.Rider;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Vehicle<SomeTypeOfRider extends Rider> implements NoiseMaker, Rideable {
-    List<SomeTypeOfRider> riders;
+    List<SomeTypeOfRider> riders = new ArrayList<>();
 
-    public void mount(SomeTypeOfRider rider){
-        riders.add(rider);
-    }
 
-    public void dismount(SomeTypeOfRider rider){
-        riders.remove(rider);
-    }
-
-    public void getNumberOfPassengers(){
-        riders.size();
-    }
     @Override
     public String makeNoise() {
         return "BEEP BEEP!";
+    }
+
+    @Override
+    public void mount(Rider rider) {
+        riders.add((SomeTypeOfRider) rider);
+    }
+
+    @Override
+    public void dismount(Rider rider) {
+        riders.remove(rider);
+    }
+
+    @Override
+    public List<Rider> getPassengers() {
+        return null;
     }
 }
